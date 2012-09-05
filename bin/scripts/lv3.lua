42d6541762ab60d9585e2588a9060104869d3862
fgenesis <false.genesis@googlemail.com>
2012-09-05 23:42:06 +0200

    Some testing data for all the collision stuff

--- bin/scripts/lv3.lua
+++ bin/scripts/lv3.lua
@@ -18,32 +18,55 @@ function e:update(dt)
     --self:position(x + math.random(-2, 2), y + math.random(-2, 2))
 end
 
-local q = quad.new("testx.png", 28):scale(4, 4)
+local q = quad.new("debug/pixels.png", 28):scale(4, 4)
+local q2 = quad.new("debug/pixels.png", 28):scale(4, 4):rotate(360, 0.6, -1)
 
 local w = entity.new()
 w:setCircleCollider(30)
 --w:setAABBCollider(-20, -20, 20, 20)
 --w:setLineCollider(40, -50, -30, 20)
 --w:rotate(360, 5, -1)
+
+local vq = quad.new("debug/vector.png", 28):alpha(0)
+
+local RADTODEG = 180.0 / 3.14159265359
+local DEGTORAD = 3.14159265359 / 180.0
+
+local function vector_getAngleDeg(vx, vy)
+    return (math.atan2(vy, vx) * RADTODEG) + 90
+end
+
 function w:update(dt)
     self:position(getMouseWorldPos())
     
-    --[[local c, x, y = self:collideWith(e)
+    local c, x, y = self:collideWith(e)
     if c then
         q:position(x, y)
         q:alpha(1)
-        camera.shake(30, 0.5)
+        --camera.shake(30, 0.5)
     else
         q:alpha(0)
-    end]]
+    end
     
     local c, x, y = self:collideGrid()
     if c then
-        q:position(x, y)
-        q:alpha(1)
+        q2:position(x, y)
+        q2:alpha(1)
         --camera.shake(10, 0.5)
     else
-        q:alpha(0)
+        q2:alpha(0)
+    end
+    
+    if isMouseButton(1) then
+        local mx, my = getMouseWorldPos()
+        local wx, wy = getWallNormal(mx, my, 30, 5)
+        if wx == 0 and wy == 0 then
+            vq:alpha(0)
+        else
+            vq:alpha(1)
+            vq:position(mx, my)
+            vq:rotate(vector_getAngleDeg(wx, wy))
+        end
     end
 end
 
